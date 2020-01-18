bc1192ccadc88f4ba479fe2a47dca9e7ae775d66
Jacek Płocharczyk <jacek.plocharczyk@protonmail.com>
2020-01-18 15:42:55 +0000

    wav2vec

---
+++ fairseq/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get install g++
+
+git clone https://github.com/pytorch/fairseq
+cd fairseq
+pip install --editable .
+pip install torchaudio
+# if there is a bug you can reinstall torchvision pip uninstall torchvision; pip install torchvision==0.3.0
+wget https://dl.fbaipublicfiles.com/fairseq/wav2vec/wav2vec_large.pt
