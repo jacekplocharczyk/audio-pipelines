4f01259c7ffec6e52d780e255867278fcaa0e701
Paweł Żuk <pzuk@0x04.net>
2020-01-19 05:55:42 +0100

    torchaudio spectogram

--- spectrogram.py
+++ spectrogram.py
@@ -3,31 +3,40 @@
 import soundfile as sf
 import glob
 from matplotlib import pyplot as plt
+import torch
+import torchaudio
 import sys
+#
+#def wav2img(wav_path, figsize=(4,4)):
+#    """
+#    takes in wave file path
+#    and the fig size. Default 4,4 will make images 288 x 288
+#    """
+##     fs = 44100 # sampling frequency
+#
+#    # use soundfile library to read in the wave files
+#    test_sound, samplerate = sf.read(wav_path)
+#
+#    # make the plot
+#    fig = plt.figure(figsize=figsize)
+#    S, freqs, bins, im = plt.specgram(test_sound, NFFT=1024, Fs=samplerate, noverlap=512)
+#    plt.show
+#    plt.axis('off')
+#
+#    ## create output path
+#    output_file = wav_path.split('/')
+#    output_file[-1] = output_file[-1].split('.wav')[0]
+#    output_file = '/'.join(output_file)
+##     print('%s -> %s' % (wav_path, output_file))
+#    plt.savefig('%s.png' % output_file)
+#    plt.close()
 
-def wav2img(wav_path, figsize=(4,4)):
-    """
-    takes in wave file path
-    and the fig size. Default 4,4 will make images 288 x 288
-    """
-#     fs = 44100 # sampling frequency
 
-    # use soundfile library to read in the wave files
-    test_sound, samplerate = sf.read(wav_path)
-
-    # make the plot
-    fig = plt.figure(figsize=figsize)
-    S, freqs, bins, im = plt.specgram(test_sound, NFFT=1024, Fs=samplerate, noverlap=512)
-    plt.show
-    plt.axis('off')
-
-    ## create output path
-    output_file = wav_path.split('/')
-    output_file[-1] = output_file[-1].split('.wav')[0]
-    output_file = '/'.join(output_file)
-#     print('%s -> %s' % (wav_path, output_file))
-    plt.savefig('%s.png' % output_file)
-    plt.close()
+def wav2img(wav_path):
+    waveform, sample_rate = torchaudio.load(wav_path)
+    specgram = torchaudio.transforms.Spectrogram()(waveform)
+    dest = wav_path[:-4] + '-spec.np'
+    specgram.numpy().dump(dest)
 
 if __name__ == '__main__':
     for x in sys.argv[1:]:
